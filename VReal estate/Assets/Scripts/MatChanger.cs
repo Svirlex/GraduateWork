84772355fa470847b2bc1d29eb24429cec8d0479
Svirlex <aleksandrsviridov446@gmail.com>
2023-03-14 21:48:28 +0300

    Working lamp and matChanger

--- VReal estate/Assets/Scripts/MatChanger.cs
+++ VReal estate/Assets/Scripts/MatChanger.cs
@@ -4,41 +4,43 @@ using UnityEngine;
 
 public class MatChanger : MonoBehaviour
 {
-
-
+    [SerializeField] Dictionary<int, Material[]> _matSets = new Dictionary<int, Material[]>();
+    [SerializeField] Material[] _materialSet0;
     [SerializeField] Material[] _materialSet1;
-    [SerializeField] Material[] _materialSet2;
-    [SerializeField] int chosenMatSet;
-    
     private int _counter = 0;
-    Dictionary<int, Material[]> matSets = new Dictionary<int, Material[]>();
-
-
-    
+    public int chosenSet;
+    [SerializeField] int customSetsAmount;
 
     void Start()
     {
-        
-        matSets.Add(0, _materialSet1);
-        matSets.Add(1, _materialSet1);
-        
+        _matSets.Add(0, _materialSet0);
+        _matSets.Add(1, _materialSet1);
+
+        for (int i = 0; i < customSetsAmount; i++)
+        {
+            _matSets.Add(1 + i, new Material[3]);
+        }
     }
 
-    private void Update()
+
+    void Update()
     {
+
+
+
         foreach (Transform _material in this.transform)
         {
             Transform oim = transform.Find("Material" + _counter.ToString());
 
             foreach (Transform _objname in oim)
             {
-                _objname.GetComponent<Renderer>().material = this.GetComponent<MatChanger>().matSets[chosenMatSet][_counter];
+                _objname.GetComponent<Renderer>().material = _matSets[chosenSet][_counter];
             }
             _counter += 1;
         }
         _counter = 0;
 
-    }
 
+    }
 }
 
